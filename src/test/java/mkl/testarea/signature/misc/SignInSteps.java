d961ac7bdf90dfd76ed386590735c4bbfad5d539
mkl <mkl@wir-sind-cool.org>
2015-10-27 18:02:49 +0100

    Additional SignInSteps test using the private key
    key.pk8
    https://drive.google.com/file/d/0By5mGrZXPqY-d0lIcmhhNGJjWjQ/view?usp=sharing
    
    eventually shared by the OP.
    
    Furthermore added some detail outputs.

--- src/test/java/mkl/testarea/signature/misc/SignInSteps.java
+++ src/test/java/mkl/testarea/signature/misc/SignInSteps.java
@@ -3,14 +3,21 @@ package mkl.testarea.signature.misc;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
 import java.security.KeyStore;
 import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.Security;
 import java.security.Signature;
 import java.security.cert.Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
 
+import org.apache.commons.io.IOUtils;
+import org.bouncycastle.asn1.ASN1Null;
+import org.bouncycastle.asn1.DERNull;
 import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.asn1.x509.DigestInfo;
@@ -35,6 +42,8 @@ public class SignInSteps
     public static PrivateKey pk = null;
     public static Certificate[] chain = null;
 
+    public static PrivateKey pkGreenhand = null;
+
     @BeforeClass
     public static void setUp() throws Exception
     {
@@ -49,6 +58,45 @@ public class SignInSteps
         String alias = (String) ks.aliases().nextElement();
         pk = (PrivateKey) ks.getKey(alias, PASSWORD);
         chain = ks.getCertificateChain(alias);
+
+        try ( InputStream keyResource = SignInSteps.class.getResourceAsStream("key.pk8"))
+        {
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(IOUtils.toByteArray(keyResource));
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            pkGreenhand = keyFactory.generatePrivate(keySpec);
+        }
+    }
+
+    /**
+     * <a href="http://stackoverflow.com/questions/33305800/difference-between-sha256withrsa-and-sha256-then-rsa">
+     * Difference between SHA256withRSA and SHA256 then RSA
+     * </a>
+     * <p>
+     * This method applies the OP's code to keystores/demo-rsa2048.ks.
+     * </p>
+     */
+    @Test
+    public void testAsGreenhandAlternativeKey() throws GeneralSecurityException, IOException
+    {
+        System.out.println("\nUsing keystores/demo-rsa2048.ks:\n================================");
+        testAsGreenhandOriginal(pk);
+        testAsGreenhandUpdated(pk);
+    }
+
+    /**
+     * <a href="http://stackoverflow.com/questions/33305800/difference-between-sha256withrsa-and-sha256-then-rsa">
+     * Difference between SHA256withRSA and SHA256 then RSA
+     * </a>
+     * <p>
+     * This method applies the OP's code to his key.
+     * </p>
+     */
+    @Test
+    public void testAsGreenhandGreenhandKey() throws GeneralSecurityException, IOException
+    {
+        System.out.println("\nUsing key.pk8:\n==============");
+        testAsGreenhandOriginal(pkGreenhand);
+        testAsGreenhandUpdated(pkGreenhand);
     }
 
     /**
@@ -59,8 +107,7 @@ public class SignInSteps
      * This method is the original code provided by the OP. As expected it shows two different signatures.
      * </p>
      */
-    @Test
-    public void testAsGreenhandOriginal() throws GeneralSecurityException, IOException
+    public void testAsGreenhandOriginal(PrivateKey privateKey) throws GeneralSecurityException, IOException
     {
         System.out.println("\nGreenhandOriginal:");
 
@@ -69,12 +116,12 @@ public class SignInSteps
         messageDigest.update(s.getBytes());
         byte[] outputDigest = messageDigest.digest();       
         //sign SHA256 with RSA
-        PrivateKey privateKey = pk;//Share.loadPk8("D:/key.pk8");
         Signature rsaSignature = Signature.getInstance("RSA");
         rsaSignature.initSign(privateKey);
         rsaSignature.update(outputDigest);
         byte[] signed = rsaSignature.sign();
         System.out.println(bytesToHex(signed));
+        System.out.println("    hash: " + bytesToHex(outputDigest));
 
 
         //compute SHA256withRSA as a single step
@@ -94,8 +141,7 @@ public class SignInSteps
      * The OP's observations seem to differ, though.
      * </p>
      */
-    @Test
-    public void testAsGreenhandUpdated() throws GeneralSecurityException, IOException
+    public void testAsGreenhandUpdated(PrivateKey privateKey) throws GeneralSecurityException, IOException
     {
         System.out.println("\nGreenhandUpdated:");
 
@@ -104,16 +150,18 @@ public class SignInSteps
         messageDigest.update(s.getBytes());
         byte[] outputDigest = messageDigest.digest();
 
-        AlgorithmIdentifier sha256Aid = new AlgorithmIdentifier(NISTObjectIdentifiers.id_sha256, null);
+        AlgorithmIdentifier sha256Aid = new AlgorithmIdentifier(NISTObjectIdentifiers.id_sha256, DERNull.INSTANCE);
         DigestInfo di = new DigestInfo(sha256Aid, outputDigest);
         //sign SHA256 with RSA
-        PrivateKey privateKey = pk;//Share.loadPk8("D:/key.pk8");
         Signature rsaSignature = Signature.getInstance("RSA");
         rsaSignature.initSign(privateKey);
-        rsaSignature.update(di.toASN1Primitive().getEncoded());
+        byte[] encodedDigestInfo = di.toASN1Primitive().getEncoded();
+        rsaSignature.update(encodedDigestInfo);
         byte[] signed = rsaSignature.sign();
         System.out.println("method 1: "+bytesToHex(signed));
-
+        System.out.println("    hash: " + bytesToHex(outputDigest));
+        System.out.println("    algo: " + sha256Aid.getAlgorithm());
+        System.out.println("    info: " + bytesToHex(encodedDigestInfo));
 
         //compute SHA256withRSA as a single step
         Signature rsaSha256Signature = Signature.getInstance("SHA256withRSA");
