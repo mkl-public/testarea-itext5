02f5d47948375b8cf0dd6921215233d241c4184d
mkl <mkl@wir-sind-cool.org>
2016-02-18 15:15:14 +0100

    New RedactText test testRedactJavishsTest1 focusing on the stackoverflow question
    iText - Cleaning Up Text in Rectangle without cleaning full row
    http://stackoverflow.com/questions/35374912/itext-cleaning-up-text-in-rectangle-without-cleaning-full-row"
    Test1.pdf
    https://www.dropbox.com/s/zeljo2k8tly7yqi/Test1.pdf?dl=0
    
    With this new file the issue could be reproduced, indeed an iText issue.

--- src/test/java/mkl/testarea/itext5/pdfcleanup/RedactText.java
+++ src/test/java/mkl/testarea/itext5/pdfcleanup/RedactText.java
@@ -46,7 +46,7 @@ public class RedactText
      * </p>
      */
     @Test
-    public void testRedactJavishsText() throws IOException, DocumentException
+    public void testRedactJavishsInput() throws IOException, DocumentException
     {
         try (   InputStream resource = getClass().getResourceAsStream("input.pdf");
                 OutputStream result = new FileOutputStream(new File(OUTPUTDIR, "input-redactedJavish.pdf")) )
@@ -59,6 +59,45 @@ public class RedactText
             linkBounds.add(1, (float) 547.3);
             linkBounds.add(2, (float) 263.3);
             linkBounds.add(3, (float) 558.4);
+
+            Rectangle linkLocation1 = new Rectangle(linkBounds.get(0), linkBounds.get(1), linkBounds.get(2), linkBounds.get(3));
+            List<PdfCleanUpLocation> cleanUpLocations = new ArrayList<PdfCleanUpLocation>();
+            cleanUpLocations.add(new PdfCleanUpLocation(1, linkLocation1, BaseColor.GRAY));
+
+            PdfCleanUpProcessor cleaner = new PdfCleanUpProcessor(cleanUpLocations, stamper);
+            cleaner.cleanUp();
+
+            stamper.close();
+            reader.close();
+        }
+    }
+
+    /**
+     * <a href="http://stackoverflow.com/questions/35374912/itext-cleaning-up-text-in-rectangle-without-cleaning-full-row">
+     * iText - Cleaning Up Text in Rectangle without cleaning full row
+     * </a>
+     * <br/>
+     * <a href="https://www.dropbox.com/s/zeljo2k8tly7yqi/Test1.pdf?dl=0">
+     * Test1.pdf
+     * </a>
+     * <p>
+     * With this new file the issue could be reproduced, indeed an iText issue.
+     * </p>
+     */
+    @Test
+    public void testRedactJavishsTest1() throws IOException, DocumentException
+    {
+        try (   InputStream resource = getClass().getResourceAsStream("Test1.pdf");
+                OutputStream result = new FileOutputStream(new File(OUTPUTDIR, "Test1-redactedJavish.pdf")) )
+        {
+            PdfReader reader = new PdfReader(resource);
+            PdfStamper stamper = new PdfStamper(reader, result);
+
+            List<Float> linkBounds = new ArrayList<Float>();
+            linkBounds.add(0, (float) 202.3);
+            linkBounds.add(1, (float) 588.6);
+            linkBounds.add(2, (float) 265.8);
+            linkBounds.add(3, (float) 599.7);
 
             Rectangle linkLocation1 = new Rectangle(linkBounds.get(0), linkBounds.get(1), linkBounds.get(2), linkBounds.get(3));
             List<PdfCleanUpLocation> cleanUpLocations = new ArrayList<PdfCleanUpLocation>();
