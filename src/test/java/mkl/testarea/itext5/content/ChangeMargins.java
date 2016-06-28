b9ea7eb4b868faaa47880ae431cdce15ddc57f5a
mkl <mkl@wir-sind-cool.org>
2016-06-28 10:56:25 +0200

    Changed ChangeMargins test testChangingMargins focusing on the stackoverflow question
    itextpdf different margin on specific page
    http://stackoverflow.com/questions/38057241/itextpdf-different-margin-on-specific-page
    
    Changed the text to demonstrate flexible vertical margins, too.

--- src/test/java/mkl/testarea/itext5/content/ChangeMargins.java
+++ src/test/java/mkl/testarea/itext5/content/ChangeMargins.java
@@ -49,9 +49,10 @@ public class ChangeMargins
             PdfWriter.getInstance(pdfDocument, pdfStream);
             pdfDocument.open();
 
-            for (int m = 0; m < pdfDocument.getPageSize().getWidth() / 2; m += 100)
+            for (int m = 0; m < pdfDocument.getPageSize().getWidth() / 2 && m < pdfDocument.getPageSize().getHeight() / 2; m += 100)
             {
-                pdfDocument.setMargins(m, m, 100, 100);
+                // pdfDocument.setMargins(m, m, 100, 100);
+                pdfDocument.setMargins(m, m, m, m);
                 pdfDocument.newPage();
                 pdfDocument.add(new Paragraph(test));
             }
