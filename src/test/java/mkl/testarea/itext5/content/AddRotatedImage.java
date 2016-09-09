6bd6eaa09ca435d2aeafd2e1ee59210990900423
mkl <mkl@wir-sind-cool.org>
2016-09-09 15:07:00 +0200

    New test AddRotatedImage focusing on the stackoverflow question
    How to rotate around the image center by itext?
    http://stackoverflow.com/questions/39364197/how-to-rotate-around-the-image-center-by-itext
    
    This test draws an image at given coordinates without rotation and then
    again as if that image was rotated around its center by some angle.

---
+++ src/test/java/mkl/testarea/itext5/content/AddRotatedImage.java
@@ -0,0 +1,85 @@
+package mkl.testarea.itext5.content;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.itextpdf.awt.geom.AffineTransform;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfWriter;
+
+/**
+ * @author mkl
+ */
+public class AddRotatedImage
+{
+    final static File RESULT_FOLDER = new File("target/test-outputs", "content");
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+        RESULT_FOLDER.mkdirs();
+    }
+
+    /**
+     * <a href="http://stackoverflow.com/questions/39364197/how-to-rotate-around-the-image-center-by-itext">
+     * How to rotate around the image center by itext?
+     * </a>
+     * <p>
+     * This test draws an image at given coordinates without rotation and then again
+     * as if that image was rotated around its center by some angle.
+     * </p>
+     */
+    @Test
+    public void testAddRotatedImage() throws IOException, DocumentException
+    {
+        try (   FileOutputStream stream = new FileOutputStream(new File(RESULT_FOLDER, "rotatedImage.pdf"))    )
+        {
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.getInstance(document, stream);
+            document.open();
+
+            PdfContentByte contentByte = writer.getDirectContent();
+
+            int x = 200;
+            int y = 300;
+            float rotate = (float) Math.PI / 3;
+
+            try (InputStream imageStream = getClass().getResourceAsStream("/mkl/testarea/itext5/layer/Willi-1.jpg "))
+            {
+                Image image = Image.getInstance(IOUtils.toByteArray(imageStream));
+
+                // Draw image at x,y without rotation
+                contentByte.addImage(image, image.getWidth(), 0, 0, image.getHeight(), x, y);
+
+                // Draw image as if the previous image was rotated around its center
+                // Image starts out being 1x1 with origin in lower left
+                // Move origin to center of image
+                AffineTransform A = AffineTransform.getTranslateInstance(-0.5, -0.5);
+                // Stretch it to its dimensions
+                AffineTransform B = AffineTransform.getScaleInstance(image.getWidth(), image.getHeight());
+                // Rotate it
+                AffineTransform C = AffineTransform.getRotateInstance(rotate);
+                // Move it to have the same center as above
+                AffineTransform D = AffineTransform.getTranslateInstance(x + image.getWidth()/2, y + image.getHeight()/2);
+                // Concatenate
+                AffineTransform M = (AffineTransform) A.clone();
+                M.preConcatenate(B);
+                M.preConcatenate(C);
+                M.preConcatenate(D);
+                //Draw
+                contentByte.addImage(image, M);
+            }
+
+            document.close();
+        }
+    }
+}
