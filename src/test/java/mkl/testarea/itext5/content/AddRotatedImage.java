fcb1f19f399a65c987baff4e25185d02c0722893
mkl <mkl@wir-sind-cool.org>
2016-09-20 12:41:17 +0200

    New AddRotatedImage test testAddRotatedFlippedImage focusing on the stackoverflow question
    How to rotate around the image center by itext?
    http://stackoverflow.com/questions/39364197/how-to-rotate-around-the-image-center-by-itext
    
    This test draws an image at given coordinates without rotation and then
    again as if that image was flipped and rotated around its center by some
    angle.

--- src/test/java/mkl/testarea/itext5/content/AddRotatedImage.java
+++ src/test/java/mkl/testarea/itext5/content/AddRotatedImage.java
@@ -82,4 +82,61 @@ public class AddRotatedImage
             document.close();
         }
     }
+
+    /**
+     * <a href="http://stackoverflow.com/questions/39364197/how-to-rotate-around-the-image-center-by-itext">
+     * How to rotate around the image center by itext?
+     * </a>
+     * <p>
+     * This test draws an image at given coordinates without rotation and then again
+     * as if that image was flipped and rotated around its center by some angle.
+     * </p>
+     */
+    @Test
+    public void testAddRotatedFlippedImage() throws IOException, DocumentException
+    {
+        try (   FileOutputStream stream = new FileOutputStream(new File(RESULT_FOLDER, "rotatedFlippedImage.pdf"))    )
+        {
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.getInstance(document, stream);
+            document.open();
+
+            PdfContentByte contentByte = writer.getDirectContent();
+
+            int x = 200;
+            int y = 300;
+            float rotate = (float) Math.PI / 3;
+
+            try (InputStream imageStream = getClass().getResourceAsStream("/mkl/testarea/itext5/layer/Willi-1.jpg "))
+            {
+                Image image = Image.getInstance(IOUtils.toByteArray(imageStream));
+
+                // Draw image at x,y without rotation
+                contentByte.addImage(image, image.getWidth(), 0, 0, image.getHeight(), x, y);
+
+                // Draw image as if the previous image was rotated around its center
+                // Image starts out being 1x1 with origin in lower left
+                // Move origin to center of image
+                AffineTransform A = AffineTransform.getTranslateInstance(-0.5, -0.5);
+                // Flip it horizontally
+                AffineTransform B = new AffineTransform(-1, 0, 0, 1, 0, 0);
+                // Stretch it to its dimensions
+                AffineTransform C = AffineTransform.getScaleInstance(image.getWidth(), image.getHeight());
+                // Rotate it
+                AffineTransform D = AffineTransform.getRotateInstance(rotate);
+                // Move it to have the same center as above
+                AffineTransform E = AffineTransform.getTranslateInstance(x + image.getWidth()/2, y + image.getHeight()/2);
+                // Concatenate
+                AffineTransform M = (AffineTransform) A.clone();
+                M.preConcatenate(B);
+                M.preConcatenate(C);
+                M.preConcatenate(D);
+                M.preConcatenate(E);
+                //Draw
+                contentByte.addImage(image, M);
+            }
+
+            document.close();
+        }
+    }
 }
