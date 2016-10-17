09d259ec5f87aaef780621b5bf19f606b702e08c
mklink <Michael.Klink@exceet.de>
2016-10-17 11:46:59 +0200

    New AddRotatedImage test testExtractStyledFromExampleDocument focusing on the stackoverflow question
    How to rotate around the image center by itext?
    http://stackoverflow.com/questions/39364197/how-to-rotate-around-the-image-center-by-itext
    
    This test draws an image at given coordinates without rotation and then
    again as if that image was rotated around its center by some angle. This
    is first done without interpolation and then with interpolation.

--- src/test/java/mkl/testarea/itext5/content/AddRotatedImage.java
+++ src/test/java/mkl/testarea/itext5/content/AddRotatedImage.java
@@ -53,7 +53,7 @@ public class AddRotatedImage
             int y = 300;
             float rotate = (float) Math.PI / 3;
 
-            try (InputStream imageStream = getClass().getResourceAsStream("/mkl/testarea/itext5/layer/Willi-1.jpg "))
+            try (InputStream imageStream = getClass().getResourceAsStream("/mkl/testarea/itext5/layer/Willi-1.jpg"))
             {
                 Image image = Image.getInstance(IOUtils.toByteArray(imageStream));
 
@@ -107,14 +107,14 @@ public class AddRotatedImage
             int y = 300;
             float rotate = (float) Math.PI / 3;
 
-            try (InputStream imageStream = getClass().getResourceAsStream("/mkl/testarea/itext5/layer/Willi-1.jpg "))
+            try (InputStream imageStream = getClass().getResourceAsStream("/mkl/testarea/itext5/layer/Willi-1.jpg"))
             {
                 Image image = Image.getInstance(IOUtils.toByteArray(imageStream));
 
                 // Draw image at x,y without rotation
                 contentByte.addImage(image, image.getWidth(), 0, 0, image.getHeight(), x, y);
 
-                // Draw image as if the previous image was rotated around its center
+                // Draw image as if the previous image was flipped and rotated around its center
                 // Image starts out being 1x1 with origin in lower left
                 // Move origin to center of image
                 AffineTransform A = AffineTransform.getTranslateInstance(-0.5, -0.5);
@@ -138,5 +138,73 @@ public class AddRotatedImage
 
             document.close();
         }
+    }
+
+    /**
+     * <a href="http://stackoverflow.com/questions/39364197/how-to-rotate-around-the-image-center-by-itext">
+     * How to rotate around the image center by itext?
+     * </a>
+     * <p>
+     * This test draws an image at given coordinates without rotation and then again
+     * as if that image was rotated around its center by some angle. This is first done
+     * without interpolation and then with interpolation.
+     * </p>
+     */
+    @Test
+    public void testAddRotatedInterpolatedImage() throws IOException, DocumentException
+    {
+        try (   FileOutputStream stream = new FileOutputStream(new File(RESULT_FOLDER, "rotatedInterpolatedImage.pdf"))    )
+        {
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.getInstance(document, stream);
+            document.open();
+
+            PdfContentByte contentByte = writer.getDirectContent();
+
+            int width = 200;
+            int height = 200;
+            float rotate = (float) Math.PI / 3;
+
+            try (InputStream imageStream = getClass().getResourceAsStream("2x2colored.png"))
+            {
+                Image image = Image.getInstance(IOUtils.toByteArray(imageStream));
+
+                addRotatedImage(contentByte, image, 200, 500, width, height, rotate);
+            }
+
+            try (InputStream imageStream = getClass().getResourceAsStream("2x2colored.png"))
+            {
+                Image image = Image.getInstance(IOUtils.toByteArray(imageStream));
+                image.setInterpolation(true);
+
+                addRotatedImage(contentByte, image, 200, 100, width, height, rotate);
+            }
+
+            document.close();
+        }
+    }
+
+    void addRotatedImage(PdfContentByte contentByte, Image image, float x, float y, float width, float height, float rotation) throws DocumentException
+    {
+        // Draw image at x,y without rotation
+        contentByte.addImage(image, width, 0, 0, height, x, y);
+
+        // Draw image as if the previous image was rotated around its center
+        // Image starts out being 1x1 with origin in lower left
+        // Move origin to center of image
+        AffineTransform A = AffineTransform.getTranslateInstance(-0.5, -0.5);
+        // Stretch it to its dimensions
+        AffineTransform B = AffineTransform.getScaleInstance(width, height);
+        // Rotate it
+        AffineTransform C = AffineTransform.getRotateInstance(rotation);
+        // Move it to have the same center as above
+        AffineTransform D = AffineTransform.getTranslateInstance(x + width/2, y + height/2);
+        // Concatenate
+        AffineTransform M = (AffineTransform) A.clone();
+        M.preConcatenate(B);
+        M.preConcatenate(C);
+        M.preConcatenate(D);
+        //Draw
+        contentByte.addImage(image, M);
     }
 }
