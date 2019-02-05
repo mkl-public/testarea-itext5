6e286afb7746599488c9a23f6a35c7cc2543e02f
mkl <mkl@wir-sind-cool.org>
2019-02-05 21:40:10 +0100

    New CreatePdf test focusing on the stack overflow question
    Convert a String with html code to Pdf format to fill in an itext
    element like a paragraph of a Document, with itext5, xmlworker
    https://stackoverflow.com/questions/54317461/convert-a-string-with-html-code-to-pdf-format-to-fill-in-an-itext-element-like-a
    
    This test shows how to convert a HTML string to a Paragraph.

--- src/test/java/mkl/testarea/itext5/xmlworker/CreatePdf.java
+++ src/test/java/mkl/testarea/itext5/xmlworker/CreatePdf.java
@@ -13,7 +13,10 @@ import org.junit.Test;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.ElementList;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
 
 /**
@@ -60,6 +63,36 @@ public class CreatePdf
             // step 4
             XMLWorkerHelper.getInstance().parseXHtml(writer, document, resource, Charset.forName("UTF8"));
             // step 5
+            document.close();
+        }
+    }
+
+    /**
+     * <a href="https://stackoverflow.com/questions/54317461/convert-a-string-with-html-code-to-pdf-format-to-fill-in-an-itext-element-like-a">
+     * Convert a String with html code to Pdf format to fill in an itext element like a paragraph of a Document, with itext5, xmlworker
+     * </a>
+     * <p>
+     * This test shows how to convert a HTML string to a Paragraph.
+     * </p>
+     */
+    @Test
+    public void testHtmlToParagraph() throws IOException, DocumentException
+    {
+        try (   FileOutputStream result = new FileOutputStream(new File(RESULT_FOLDER, "HtmlToParagraph.pdf")))
+        {
+            String html = "<h1>Hello</h1><br/>\n" + 
+                    "<h3>This is a test to demonstrate a simple html code I just need to convert</h3>";
+            String css = "h1 { background-color: lightblue; font-size: 20pt} h3 {font-family: verdana; text-align: center; color: red;}";
+            ElementList elements = XMLWorkerHelper.parseToElementList(html, css);
+            Paragraph paragraph = new Paragraph();
+            for (Element element : elements) {
+                paragraph.add(element);
+            }
+
+            Document document = new Document();
+            PdfWriter.getInstance(document, result);
+            document.open();
+            document.add(paragraph);
             document.close();
         }
     }
