8bc28a64f9c153e1bee2b2f4c0b532be44be818c
mkl <mkl@wir-sind-cool.org>
2019-09-13 10:16:33 +0200

    New CreateStamp test focusing on the OpenPDF issue 253
    Issue with createStamp annotation on Adobe Acrobat Reader DC
    https://github.com/LibrePDF/OpenPDF/issues/253
    
    testCreateMultipleLikeDprutean - In contrast to what the OP claims to
    have found, i.e. that the size of the template changes the position of
    the yellow balloon icon, the output of this test shows no influence of
    the template size on that position.

--- src/test/java/mkl/testarea/itext5/annotate/CreateStamp.java
+++ src/test/java/mkl/testarea/itext5/annotate/CreateStamp.java
@@ -52,4 +52,44 @@ public class CreateStamp {
         document.close();
     }
 
+    /**
+     * <a href="https://github.com/LibrePDF/OpenPDF/issues/253">
+     * Issue with createStamp annotation on Adobe Acrobat Reader DC
+     * </a>
+     * <p>
+     * In contrast to what the OP claims to have found, i.e.
+     * that the size of the template changes the position of
+     * the yellow balloon icon, the output of this test shows
+     * no influence of the template size on that position.
+     * </p>
+     * @see #addStamp(PdfWriter, float, float, float, float, float, float)
+     */
+    @Test
+    public void testCreateMultipleLikeDprutean() throws IOException, DocumentException {
+        Document document = new Document();
+        PdfWriter pdfWriter = PdfWriter.getInstance(document,
+                new FileOutputStream(new File(RESULT_FOLDER, "StampMultipleLikeDprutean.pdf")));
+        document.open();
+        document.add(new Paragraph("This is a test document"));
+
+        addStamp(pdfWriter, 100, 600, 200, 700, 50, 50);
+        addStamp(pdfWriter, 100, 400, 200, 500, 100, 100);
+        addStamp(pdfWriter, 100, 200, 200, 300, 200, 200);
+
+        addStamp(pdfWriter, 300, 600, 450, 750, 50, 50);
+        addStamp(pdfWriter, 300, 400, 450, 550, 100, 100);
+        addStamp(pdfWriter, 300, 200, 450, 350, 200, 200);
+
+        document.close();
+    }
+
+    /** @see #testCreateMultipleLikeDprutean() */
+    void addStamp(PdfWriter pdfWriter, float llx, float lly, float urx, float ury, float width, float height) {
+        PdfAnnotation annotation = PdfAnnotation.createStamp(pdfWriter, new Rectangle(llx, lly, urx, ury),
+                String.format("(%3.1f,%3.1f) to (%3.1f,%3.1f), template %3.1f\u00D7%3.1f", llx, lly, urx, ury, width, height),
+                "#Comment");
+        annotation.setAppearance(PdfName.N, pdfWriter.getDirectContent().createTemplate(width, height));
+        pdfWriter.addAnnotation(annotation);
+
+    }
 }
