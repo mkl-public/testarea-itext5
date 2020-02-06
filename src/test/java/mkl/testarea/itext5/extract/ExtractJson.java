0e4b6384cd43bcaa6eec39c63fae1e5b8a267dd8
mkl <mkl@wir-sind-cool.org>
2020-02-06 14:56:20 +0100

    New ExtractJson test focusing on the stack overflow question
    Error while reading json from pdf file using iText
    https://stackoverflow.com/questions/60089111/error-while-reading-json-from-pdf-file-using-itext
    Valid Json.txt
    https://drive.google.com/open?id=1ShI1CrPvktTm7us0UJ2sP0qsOJaXMU0-a-ONrfSAWcY
    
    testJsonToPdfToJsonFormField - Adding the content in a form field
    instead of in the static text content allows faithful extraction of the
    json object.

--- src/test/java/mkl/testarea/itext5/extract/ExtractJson.java
+++ src/test/java/mkl/testarea/itext5/extract/ExtractJson.java
@@ -14,8 +14,10 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.io.StreamUtil;
+import com.itextpdf.text.pdf.PdfFormField;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.TextField;
 import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 
 /**
@@ -52,6 +54,7 @@ public class ExtractJson {
      * <p>
      * Thus, one has to change the way one embeds the json data.
      * </p>
+     * @see #testJsonToPdfToJsonFormField()
      */
     @Test
     public void testJsonToPdfToJsonLikeHitendra() throws IOException, DocumentException {
@@ -85,4 +88,49 @@ public class ExtractJson {
         Files.write(extractedJsonFile.toPath(), Collections.singleton(jsonBody));
     }
 
+    /**
+     * <a href="https://stackoverflow.com/questions/60089111/error-while-reading-json-from-pdf-file-using-itext">
+     * Error while reading json from pdf file using iText
+     * </a>
+     * <br/>
+     * <a href="https://drive.google.com/open?id=1ShI1CrPvktTm7us0UJ2sP0qsOJaXMU0-a-ONrfSAWcY">
+     * Valid Json.txt
+     * </a>
+     * <p>
+     * Adding the content in a form field instead of in the static text content allows
+     * faithful extraction of the json object.
+     * </p>
+     * @see #testJsonToPdfToJsonLikeHitendra()
+     */
+    @Test
+    public void testJsonToPdfToJsonFormField() throws IOException, DocumentException {
+        String originalJson = null;
+        try (   InputStream resource = getClass().getResourceAsStream("Valid Json.txt")) {
+            byte[] resourceBytes = StreamUtil.inputStreamToArray(resource);
+            originalJson = new String(resourceBytes);
+        }
+
+        File jsonPdfFile = new File(RESULT_FOLDER, "JsonFormField.pdf");
+        File extractedJsonFile = new File(RESULT_FOLDER, "JsonFormField.txt");
+
+        Document document = new Document();
+        document.setPageSize(PageSize.A4);
+        document.addCreationDate();
+        document.addAuthor("Me");
+        PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream(jsonPdfFile));
+        document.open();
+        pdfWriter.getAcroForm().setNeedAppearances(true);
+        TextField textField = new TextField(pdfWriter, document.getPageSize(), "json");
+        textField.setOptions(TextField.MULTILINE | TextField.READ_ONLY);
+        PdfFormField field = textField.getTextField();
+        field.setValueAsString(originalJson);
+        pdfWriter.addAnnotation(field);
+        document.close();
+
+        PdfReader pdfReader = new PdfReader(jsonPdfFile.getAbsolutePath());
+        String jsonBody = pdfReader.getAcroFields().getField("json");
+        pdfReader.close();
+
+        Files.write(extractedJsonFile.toPath(), Collections.singleton(jsonBody));
+    }
 }
