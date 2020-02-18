b6c16873919ea5e149ac73a08ab5c7d05a2a9bc8
mkl <mkl@wir-sind-cool.org>
2020-02-18 19:05:25 +0100

    New CertificateInformation test focusing on the stack overflow question
    Can't pars subjectAlternativeNames in java
    https://stackoverflow.com/questions/60261855/cant-pars-subjectalternativenames-in-java
    json "leaf_input", inlined
    https://ct.googleapis.com/rocketeer/ct/v1/get-entries?start=32621134&end=32621134
    
    testAntonsCertificateNew - With the OP's code piece, it becomes clear
    that the issue is a malformed subject alternative name value, an URI
    which is no URI.

--- src/test/java/mkl/testarea/signature/misc/CertificateInformation.java
+++ src/test/java/mkl/testarea/signature/misc/CertificateInformation.java
@@ -1,24 +1,37 @@
 package mkl.testarea.signature.misc;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Base64;
+import java.util.Map;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import sun.security.util.DerValue;
+import sun.security.x509.CertificateExtensions;
+import sun.security.x509.Extension;
+import sun.security.x509.X509CertImpl;
+import sun.security.x509.X509CertInfo;
+
 /**
  * @author mkl
  */
+@SuppressWarnings("restriction")
 public class CertificateInformation {
+    final static File RESULT_FOLDER = new File("target/test-outputs", "signature");
 
-    /**
-     * @throws java.lang.Exception
-     */
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
+        RESULT_FOLDER.mkdirs();
     }
 
     /**
@@ -45,4 +58,72 @@ public class CertificateInformation {
         X509Certificate x509Certificate = (X509Certificate) certificate;
     }
 
-}
+    /**
+     * <a href="https://stackoverflow.com/questions/60261855/cant-pars-subjectalternativenames-in-java">
+     * Can't pars subjectAlternativeNames in java
+     * </a>
+     * <br/>
+     * <a href="https://ct.googleapis.com/rocketeer/ct/v1/get-entries?start=32621134&end=32621134">
+     * json "leaf_input"
+     * </a>, inlined
+     * <p>
+     * With the OP's code piece, it becomes clear that the issue is a
+     * malformed subject alternative name value, an URI which is no URI.
+     * </p>
+     */
+    @Test
+    public void testAntonsCertificateNew() throws IOException, CertificateException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+        ByteArrayInputStream in = new ByteArrayInputStream(Base64.getDecoder().decode(
+                "AAAAAAFXsGq8HwAAAAW+MIIFujCCBKKgAwIBAgIDAZZpMA0GCSqGSIb3DQEBBQUAMEUxCzAJBgNVBAYTAkxVMRYwFAYDVQQKEw1MdXhUcnVzdCBTLkEuMR4wHAYDVQQDExVMdXhUcnVzdCBRdWFsaWZpZWQgQ0EwHhcNMTAwNDE1MDY0MzM0WhcNMTMwNDE1MDY0MzM0WjB8MQswCQYDVQQGEwJMVTERMA8GA1UEBxMITXVuc2JhY2gxFDASBgNVBAoTC0NFVFJFTCBTLkEuMRAwDgYDVQQLEwdJT1AtU1NTMRQwEgYDVQQDFAsqLmNldHJlbC5sdTEcMBoGCSqGSIb3DQEJARYNc3NzQGNldHJlbC5sdTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALhf+I7RshQlHMMWq/WPDLNxx+ODMd4Tn9ej14igvMEE+RYEagdMOZoeO8Bqz9qV2atzFHqz0D+Ad+cxuznBGKl4rhS9gUejgoAMox7car0+LSsv1NT4J0gAlnmH3BJlDMd9CihT0D/sRwMNfa8GYAvCuGDtWIvYb497RFy+2kmzk3cwCk3BgOO3MsT7iqhcn65Pd1Lq1vLjCCuQBoWLlcKk4uptPsyFKrHEh1/0ksY5evqBPxioVppoN+oay20RK36JzrzAl+vfpzq03WRlM2IgM0ItnesLqid9GqTUsOTq59i5aVX1EKlfgM5v7YCpYMLrJA+JBO3beR/4FSczfccCAwEAAaOCAnowggJ2MAwGA1UdEwEB/wQCMAAwYAYIKwYBBQUHAQEEVDBSMCMGCCsGAQUFBzABhhdodHRwOi8vb2NzcC5sdXh0cnVzdC5sdTArBggrBgEFBQcwAoYfaHR0cDovL2NhLmx1eHRydXN0Lmx1L0xUUUNBLmNydDBBBgNVHREEOjA4hjZETlM6Ki5jZXRyZWwubHUsIEROUzptYWlsLmNldHJlbC5sdSwgRE5TOnd3dy5jZXRyZWwubHUwggEABgNVHSAEgfgwgfUwgegGCCuBKwEBAgYBMIHbMIGtBggrBgEFBQcCAjCBoBqBnUx1eFRydXN0IFNlcnZlciBDZXJ0aWZpY2F0ZS4gTm90IHN1cHBvcnRlZCBieSBTU0NELCBLZXkgR2VuZXJhdGlvbiBieSBTdWJzY3JpYmVyLiBHVEMsIENQIGFuZCBDUFMgb24gaHR0cDovL3JlcG9zaXRvcnkubHV4dHJ1c3QubHUuIFNpZ25lZCBieSBhIFF1YWxpZmllZCBDQS4wKQYIKwYBBQUHAgEWHWh0dHA6Ly9yZXBvc2l0b3J5Lmx1eHRydXN0Lmx1MAgGBgQAj3oBAzARBglghkgBhvhCAQEEBAMCBeAwDgYDVR0PAQH/BAQDAgSwMCcGA1UdJQQgMB4GCCsGAQUFBwMBBggrBgEFBQcDAgYIKwYBBQUHAwQwHwYDVR0jBBgwFoAUjZCjB90aE3eZTJKrTUPeP80pZAUwMQYDVR0fBCowKDAmoCSgIoYgaHR0cDovL2NybC5sdXh0cnVzdC5sdS9MVFFDQS5jcmwwHQYDVR0OBBYEFLhOdzNJg4CSchxSsHbwKWMVm8xnMA0GCSqGSIb3DQEBBQUAA4IBAQBrQ9bYRA5O8a+4vIfVx5izH5x9yieKuQjpF4Etc4YUxfk3h5yZieWJuZGHygUjV5TCYDdhtVf6FFWkJ4FT5l6zDQeOLrEWqL12qcT4hGFN61mwjZO7kca8IHlqPPeqtYVg/Ssbpun+bjPOsGdvrvMulqNNTz5UeQuovc/VFaoHpYCQhezrQ6E6uQ684f6LFVIbsah6pT58wEnrf6xE1aRdSk27e3bF8wns3zOVsWE2wKck5pMS5DkGwjWli27Aqt6QQCyCKC7xqqxwL8GfnmXZNdn2iYYfSyr0I7rdqxa7FsuNFkEb8/PdZlyMxQP867YnucRCyLzzjCnRy1bOLnUeAAA="
+        ));
+        readNumber(in, 1); // version
+        readNumber(in, 1); // type
+        readNumber(in, 8); // timestamp
+        readNumber(in, 2); // entry type
+        int length = (int) readNumber(in, 3);
+        byte[]  x509 = readFixedLength(in, length);
+        Files.write(new File(RESULT_FOLDER, "AntonsCertificate.cer").toPath(), x509);
+        X509CertImpl cert = (X509CertImpl) certFactory.generateCertificate(new ByteArrayInputStream(x509));
+        Map<String, Extension> unparseables = ((CertificateExtensions)((X509CertInfo)cert.get("x509.info")).get("extensions")).getUnparseableExtensions(); // <-- want too be empty, but it's not!!!
+
+
+        // Analysis
+        Extension extension = unparseables.get("2.5.29.17");
+        byte[] value = extension.getValue();
+        System.out.printf("Extension value: %s\n", new String(value));
+        DerValue derValue = new DerValue(value);
+        while (derValue.data.available() > 0) {
+            DerValue encName = derValue.data.getDerValue();
+            if ((encName.tag & 0x1f) == 6) {
+                encName.resetTag(DerValue.tag_IA5String);
+                System.out.printf("IA5String value from URI GeneralName value: %s\n", encName.getIA5String());
+            }
+        }
+
+        Field whyField = extension.getClass().getDeclaredField("why");
+        whyField.setAccessible(true);
+        Throwable why = (Throwable) whyField.get(extension);
+        why.printStackTrace();
+    }
+
+    static byte[] readFixedLength(InputStream inputStream, int dataLength) throws IOException {
+        byte[] toReturn = new byte[dataLength];
+        int bytesRead = inputStream.read(toReturn);
+        if (bytesRead < dataLength) {
+            throw new RuntimeException();
+        }
+        return toReturn;
+    }
+
+    static long readNumber(InputStream inputStream, int numBytes) throws IOException {
+        long toReturn = 0;
+        for (int i = 0; i < numBytes; i++) {
+            int valRead = inputStream.read();
+            if (valRead < 0) {
+                throw new RuntimeException();
+            }
+            toReturn = (toReturn << 8) | valRead;
+        }
+        return toReturn;
+    }}
