efc08c42380d42fa7216600521c78cbd3f78286f
Michael Klink <Michael.Klink@exceet.de>
2020-09-28 17:01:30 +0200

    New AddImage test focusing on the stack overflow question
    itext _ black image in pdf created
    https://stackoverflow.com/questions/64065837/itext-black-image-in-pdf-created
    printer.jpeg
    https://drive.google.com/file/d/1Qq_g7k-ngHFdHNHVeqJYuO_2dLtdH0hj/view?usp=sharing
    
    testAddImagePrinterLikeLuke - Cannot reproduce the issue, Adobe Acrobat Reader DC shows the image just fine.

--- src/test/java/mkl/testarea/itext5/content/AddImage.java
+++ src/test/java/mkl/testarea/itext5/content/AddImage.java
@@ -1,12 +1,12 @@
 package mkl.testarea.itext5.content;
 
-import static org.junit.Assert.*;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.stream.StreamSupport;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -55,4 +55,31 @@ public class AddImage {
         doc.close();
     }
 
+    /**
+     * <a href="https://stackoverflow.com/questions/64065837/itext-black-image-in-pdf-created">
+     * itext _ black image in pdf created
+     * </a>
+     * <br/>
+     * <a href="https://drive.google.com/file/d/1Qq_g7k-ngHFdHNHVeqJYuO_2dLtdH0hj/view?usp=sharing">
+     * printer.jpeg
+     * </a>
+     * <p>
+     * Cannot reproduce the issue, Adobe Acrobat Reader DC shows the image just fine.
+     * </p>
+     */
+    @Test
+    public void testAddImagePrinterLikeLuke() throws IOException, DocumentException {
+        Document document = new Document();
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(new File(RESULT_FOLDER, "PrinterImageLikeLuke.pdf")));
+        document.open();
+
+        byte[] byteArray = null;
+        try (   InputStream imageResource = getClass().getResourceAsStream("printer.jpeg") ) {
+            byteArray = StreamUtil.inputStreamToArray(imageResource);
+        }
+        Image image1 = Image.getInstance(byteArray);
+
+        document.add(image1);
+        document.close();
+    }
 }
