aa151ecc9ecba417b9502dab2168bb97033373fd
mkl <klink@csi.com>
2021-07-01 17:58:11 +0200

    New AddSpecialCharacters test focusing on the stack overflow question 68206055
    Helvetica from itext doesn't show all helvetica characters?
    https://stackoverflow.com/questions/68206055/helvetica-from-itext-doesnt-show-all-helvetica-characters
    
    testAddLikeLouis33 - Cannot reproduce the issue.

--- src/test/java/mkl/testarea/itext5/content/AddSpecialCharacters.java
+++ src/test/java/mkl/testarea/itext5/content/AddSpecialCharacters.java
@@ -1,8 +1,7 @@
 package mkl.testarea.itext5.content;
 
-import static org.junit.Assert.*;
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -12,8 +11,11 @@ import org.junit.Test;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
+import com.itextpdf.text.Font.FontFamily;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 /**
@@ -53,5 +55,40 @@ public class AddSpecialCharacters {
 
             document.close();
         }
+    }
+
+    /**
+     * <a href="https://stackoverflow.com/questions/68206055/helvetica-from-itext-doesnt-show-all-helvetica-characters">
+     * Helvetica from itext doesn't show all helvetica characters?
+     * </a>
+     * <p>
+     * Cannot reproduce the issue.
+     * </p>
+     */
+    @Test
+    public void testAddLikeLouis33() throws IOException, DocumentException {
+        try (   FileOutputStream stream = new FileOutputStream(new File(RESULT_FOLDER, "specialCharactersLikeLouis33.pdf")) )
+        {
+            Document document = new Document();
+            PdfWriter.getInstance(document, stream);
+            document.open();
+
+            PdfPTable table = new PdfPTable(1);
+            table.addCell(createCellLikeLouis33("Apostrophes: `'´"));
+            table.addCell(createCellLikeLouis33("Ellipsis: …"));
+            document.add(table);
+
+            document.close();
+        }
+    }
+
+    PdfPCell createCellLikeLouis33(String val) {
+        Font font = new Font(FontFamily.HELVETICA);
+        font.setSize(12);
+        PdfPCell cell = new PdfPCell(new Paragraph(val, font));
+        cell.setMinimumHeight(20);
+        cell.setIndent(30);
+        cell.setFollowingIndent(30);
+        return cell;
     }
 }
